80424e54662eeca8b682e0a7dfbd583b2f058d74
sbrycbc <sabriyecbc@gmail.com>
2025-01-07 10:31:36 +0100

    added product and category model...

---
+++ src/app/model/rest.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from './product.model';
+import { Category } from './category.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestService {
+
+  baseUrl: string = "http://localhost:3500/";
+
+  constructor(private http: HttpClient) { }
+
+  getProducts() : Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl + 'products');
+  }
+  getCategory(): Observable<Category[]> {
+    return this.http.get<Category[]>(this.baseUrl + 'categories');
+  }
+}
