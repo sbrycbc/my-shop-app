4f3e77309437c0e30883cd53dc87530b3d860724
sbrycbc <sabriyecbc@gmail.com>
2025-01-07 12:32:51 +0100

    added navbar component with styling...

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -3,13 +3,14 @@ import { ModelModule } from '../model/model.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { ShopComponent } from './shop.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
 
     imports: [ModelModule, BrowserModule, FormsModule],
     providers: [],
-    declarations: [ ShopComponent],
+    declarations: [ ShopComponent, NavbarComponent],
     exports: [ ShopComponent]
 })
 
